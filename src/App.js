e8faf7e1daae1d24a714d566a49e2c48c7955c9a
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-06-01 16:28:35 +1000

    Attributes In JSX

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 function App() {
   const h1 = <h1>Hello world</h1>;
   const myArticle = <article>Hello world</article>;
+  const p1 = <p id='large'>foo</p>;
+  const p2 = <p id='small'>bar</p>;
   return (
     <>
       < p > Hello world</p >
