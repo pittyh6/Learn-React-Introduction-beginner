5ffd3d90cdc354e12851eb11a968d43d69607fe7
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-06-03 10:08:23 +1000

    nested jsx

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ function App() {
   const myArticle = <article>Hello world</article>;
   const p1 = <p id='large'>foo</p>;
   const p2 = <p id='small'>bar</p>;
+  const myDiv = (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  )
   return (
     <>
       < p > Hello world</p >
