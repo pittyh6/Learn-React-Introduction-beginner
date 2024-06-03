d3b6b999ce3283e41b7f6065749497a86ac95235
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-06-03 10:11:45 +1000

    outer elements jsx

--- src/App.js
+++ src/App.js
@@ -10,7 +10,18 @@ function App() {
     <div>
       <h1>Hello world</h1>
     </div>
-  )
+  );
+  const blog = (
+    <div>
+      <img src="pics/192940u73.jpg" />
+      <h1>
+        Welcome to Dan's Blog!
+      </h1>
+      <article>
+        Wow I had the tastiest sandwich today. I <strong>literally</strong> almost freaked out.
+      </article>
+    </div>
+  );
   return (
     <>
       < p > Hello world</p >
