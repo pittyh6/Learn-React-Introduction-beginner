6c08faa838b47fa0147fba54cd6cd3e51d5fec64
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-06-08 16:16:36 +1000

    start understand componentes

--- src/reactConponent.js
+++ src/reactConponent.js
@@ -18,12 +18,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import MyComponent from './App';
 
+ReactDOM.createRoot(document.getElementById('app')).render(<MyComponent />);
 
 
 
+//------------------------------------------------------------------
+//index.html
+< !DOCTYPE html >
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link rel="stylesheet" href="style.css">
+        </head>
+        <body>
+            <main id="app">
+            </main>
+            <script src="https://content.codecademy.com/courses/React/react-18-course-bundle.min.js"></script>
+            <script src="/index.compiled.js"></script>
+        </body>
+    </html>
 
 
-/*
+
+/** 
  * On line 1 of App.js and index.js, import React from 'react' creates a JavaScript object. This object contains properties that are needed to make React work, such as React.createElement().
 On line 2 of index.js import ReactDOM from 'react-dom/client' creates another JavaScript object. This object contains methods that help React interact with the DOM, such as ReactDOM.createRoot().
 On line 3 of App.js, by writing a JavaScript function, a function component was defined. We can’t see this component quite yet, as it’s more of a factory that produces instances of itself when used. If we want to see it, we need to render it into the DOM.
@@ -38,3 +56,16 @@ return <h1>Hello, this is a function component body.</h1>;
 
 That doesn’t look like a set of instructions explaining how to build a React component! Yet that’s exactly what it is.
 */
+
+/**
+ Before you go, here’s a recap:
+
+React applications are made up of components.
+Components are responsible for rendering pieces of the user interface.
+To create components and render them, react and reactDOM must be imported.
+React components can be defined with Javascript functions to make function components.
+Function component names must start with a capitalized letter, and Pascal case is the adopted naming convention.
+Function components must return some React elements in JSX syntax.
+React components can be exported and imported from file to file.
+A React component can be used by calling the component name in an HTML-like self-closing tag syntax.
+Rendering a React component requires using .createRoot() to specify a root container and calling the .render() method on it. */
