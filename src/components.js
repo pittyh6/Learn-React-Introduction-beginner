f1c03f18138fbbbfcda1e474b2c84d5dcd8f1aa4
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-06-10 12:51:08 +1000

    Event Listener and Event Handlers in a Component

---
+++ src/components.js
@@ -0,0 +1,21 @@
+/** 
+Review
+1 min
+Congratulations! You have finished the lesson on React components.
+
+    Here’s a short recap of the concepts introduced in this lesson:
+
+Function components can return multiple JSX lines by nesting the elements in a parent element.
+Variable attributes can be used inside of a React component with JavaScript injections.
+React components support logic by putting the logic statements above the return statements.
+Components can conditionally return JSX elements by putting conditional statements inside of the components.
+Components can respond to events by defining event handlers and passing them to the JSX elements.
+Great job on tackling these complex topics! You have spent a lot of time studying React components in isolation! Now, it’s time to learn how components fit into the world around them.
+
+    Instructions
+If you would like to practice your learned skills, consider these challenges:
+
+Add logic to the MyQuote component to produce different quotes based on different conditions.
+Add elements onto the screen that can respond to events such as clicking or hovering.Make sure to define event handlers for them within the component.
+
+*/
