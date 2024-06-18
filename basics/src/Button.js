a380a300f198cae916442c87b28abcea196cf99d
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-06-18 16:14:20 +1000

    Receive an Event Handler as a prop

--- basics/src/Button.js
+++ basics/src/Button.js
@@ -23,3 +23,17 @@ function App() {
 }
 
 export default App;
+
+
+//button.js
+import React from 'react';
+
+function Button(props) {
+    return (
+        <button onClick={props.talk}>
+            Click me!
+        </button>
+    );
+}
+
+export default Button;
