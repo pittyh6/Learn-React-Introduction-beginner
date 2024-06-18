eb96f83c0b0875df9ae57e78d7d772672a6acea8
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-06-18 16:11:26 +1000

    Pass an Event Handler as a prop

--- basics/src/Talker.js
+++ basics/src/Talker.js
@@ -34,3 +34,20 @@ function Example() {
         </h1>
     );
 }
+
+//talker.js
+import React from 'react';
+import Button from './Button';
+
+function Talker() {
+    function talk() {
+        let speech = '';
+        for (let i = 0; i < 10000; i++) {
+            speech += 'blah ';
+        }
+        alert(speech);
+    }
+    return <Button talk={talk} />;
+}
+
+export default Talker;
