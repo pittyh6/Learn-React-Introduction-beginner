64380c1497cbb47f8923882b863437e54a8c840f
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-06-18 16:00:46 +1000

    Put an Event Handler in a Function Component

---
+++ basics/src/Talker.js
@@ -0,0 +1,36 @@
+//talker.js
+import React from 'react';
+import Button from './Button';
+
+
+
+function Talker() {
+    function talk() {
+        let speech = '';
+        for (let i = 0; i < 10000; i++) {
+            speech += 'blah ';
+        }
+        alert(speech);
+    }
+    return <Button onClick={talk} />;
+}
+
+export default Talker;
+
+
+
+//example.js
+import React from 'react';
+
+function Example() {
+    function handleEvent() {
+        alert(`I am an event handler.
+      If you see this message,
+      then I have been called.`);
+    }
+    return (
+        <h1 onClick={handleEvent}>
+            Hello world
+        </h1>
+    );
+}
